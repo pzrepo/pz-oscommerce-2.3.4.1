b2da01635890b3a28d958c2c8eb3e179ec1cbeea
Jinesh <jinesh.d@paymentz.com>
2018-01-12 13:11:26 +0530

    pz-oscommerce plugin

---
+++ includes/languages/english/modules/payment/pz_oscommerce.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Version: 1.0.0
+ * Do not change any code without permission
+ */
+
+
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_TEXT_TITLE', '<div style=" display: inline-flex;">Pz OsCommerce '."[".MODULE_PAYMENT_PZ_OSCOMMERCE_DESCRIPTION."]".'</span></div>');
+
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_TEXT_DESCRIPTION', ' Please make your check or money order payable to:<br />' . MODULE_PAYMENT_PZ_OSCOMMERCE_MERCHANT_ID . '<br />');
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_TITLE_TEXT', ' Pz OsCommerce');
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_TEXT_ERROR', 'Credit Card Error!');
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_TEXT_ERROR_MESSAGE', 'There has been an error processing you credit card at Pz OsCommerce, please try again.');
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_ALERT_ERROR_MESSAGE', 'Security Error. Illegal access detected, please try again.');
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_CANCEL_ERROR_MESSAGE', 'Thank you for shopping with us.We will keep you posted regarding the status of your order through e-mail');
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_FAILED_ERROR_MESSAGE', 'Thank you for shopping with us. However your transaction failed');
+
+define('MODULE_PAYMENT_PZ_OSCOMMERCE_TEXT_EMAIL_FOOTER', "Please make your check or money order payable to:" . "\n\n" . MODULE_PAYMENT_PZ_OSCOMMERCE_MERCHANT_ID . "\n");\define('MODULE_PAYMENT_PZ_OSCOMMERCE_TEXT_DESCRIPTION', ' Enable Pz OsCommerce Online Payments');
+
+?>
+
